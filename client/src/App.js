2a5fb5bf874e3bcf5616725b4ebb3927385067f2
Rajneesh Dubey <dubeyrajneesh786@gmail.com>
2023-09-05 09:46:22 +0530

    Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './components/screen/Login';
 import Signup from './components/screen/Signup';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div >
       <Login/>
       <Signup/>
+      <Navbar/>
       
     </div>
   );
---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        Hello Navbar
+      
+    </div>
+  )
+}
+
+export default Navbar
