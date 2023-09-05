b16211b13b898cdedc1b9f73b6cbd300777452fb
Rajneesh Dubey <dubeyrajneesh786@gmail.com>
2023-09-05 09:42:39 +0530

    signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/screen/Login';
+import Signup from './components/screen/Signup';
 
 function App() {
   return (
     <div >
       <Login/>
+      <Signup/>
       
     </div>
   );
---
+++ client/src/components/screen/Signup.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div>
+        Hello signup
+      
+    </div>
+  )
+}
+
+export default Signup
